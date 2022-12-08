335f1d121e93167136146ec39d59074e3de63f3d
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-08 20:20:52 +0100

    Day 3 - Exam Bootcamp

--- src/Spiel_Demo.java
+++ src/Spiel_Demo.java
@@ -1,22 +1,54 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+// nextInt is normally exclusive of the top value,
+// so add 1 to make it inclusive
+// int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
 
 public class Spiel_Demo {
 
-    static Integer myNumber = 42;
+    static Integer myNumber = ThreadLocalRandom.current().nextInt(0, 100 + 1);
+    static Integer tries = 0;
+
+
+
 
     public static void main(String[] args) {
-        guess(42);
-        guess(13);
-        guess(42);
-        guess(94);
 
+        // Die nächste Zeile nachher auskommentieren, sonst ist es ja kein Raten mehr.. :D
+        System.out.println("The number is: " + myNumber);
+
+        nextRound();
     }
 
+    public static void nextRound() {
+
+        tries++;
+        // ist das gleiche wie tries = tries +1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine Zahl ein: ");
+
+        Integer userNumber = scanner.nextInt();
+        guess(userNumber);
+
+    }
     public static void guess(Integer userNumber) {
         if (userNumber == myNumber) {
             System.out.println("Richtig geraten!! :-)");
+            System.out.println("Du hast " + tries + " Versuche gebraucht.");
         } else {
             System.out.println("Leider falsch geraten! :-( Try again..");
         }
+
+        if (userNumber < myNumber){
+            System.out.println("Deine Zahl ist zu klein.");
+        } else if ( userNumber == myNumber) {
+            System.out.println("Supi!");
+        }
+        else {
+            System.out.println("Deine Zahl ist zu groß.");
+        }
+        nextRound();
     }
 }
