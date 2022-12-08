3939c9434e75357f53952e2d72987dd49c3b6ace
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-08 23:28:21 +0100

    Day 4 - Exam Bootcamp (UI fertig, Game works :-))

--- src/Spiel_Demo.java
+++ src/Spiel_Demo.java
@@ -13,7 +13,8 @@ public class Spiel_Demo {
 
     static Integer myNumber = ThreadLocalRandom.current().nextInt(0, 100 + 1);
     static Integer tries = 0;
-    static JLabel text = new JLabel("Gebe eine Zahl zwischen 0 und 100 ein!"   );
+    static JLabel text = new JLabel("Gebe eine Zahl zwischen 0 und 100 ein!");
+    static JTextField einzigesTextField = new JTextField();
 
 
     public static void main(String[] args) {
@@ -33,12 +34,11 @@ public class Spiel_Demo {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setDefaultLookAndFeelDecorated(true);
 
-        JLabel text = new JLabel("Gebe eine Zahl zwischen 0 und 100 ein!"   );
-        text.setBounds(50,50,300,30);
+        // auch das hier oben geaddet, deshalb hier obsolet: JLabel text = new JLabel("Gebe eine Zahl zwischen 0 und 100 ein!");
+        text.setBounds(50,50,350,30);
 
-
-        JTextField einzigesTextField = new JTextField();
-        einzigesTextField.setBounds(50,100,200,30);
+        // das stand hier bevor wir es oben geaddet haben: JTextField einzigesTextField = new JTextField();
+        einzigesTextField.setBounds(50,100,50,30);
 
         JButton button = new JButton("Raten Sie!");
         button.setBounds(50,150,200,30);
@@ -46,9 +46,13 @@ public class Spiel_Demo {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
                 String textFromEinzigesTextfield = einzigesTextField.getText();
                 Integer userNumber = Integer.parseInt(textFromEinzigesTextfield);
-                guess(userNumber);
+                guess(userNumber); }
+                catch (Exception exception){
+                    text.setText("Bitte gebe eine ZAHL ein!");
+                }
             }
         });
 
@@ -59,7 +63,7 @@ public class Spiel_Demo {
         frame.setVisible(true);
     }
 
-    public static void nextRound() {
+    /** public static void nextRound() {
 
         tries++;
         // ist das gleiche wie tries = tries +1;
@@ -68,26 +72,29 @@ public class Spiel_Demo {
         System.out.println("Bitte geben Sie eine Zahl ein: ");
 
         Integer userNumber = scanner.nextInt();
-        guess(userNumber);
+        guess(userNumber); */
 
-    }
+
     public static void guess(Integer userNumber) {
+
         if (userNumber == myNumber) {
             System.out.println("Richtig geraten!! :-)");
             System.out.println("Du hast " + tries + " Versuche gebraucht.");
-        } else {
-            System.out.println("Leider falsch geraten! :-( Try again..");
-            text.setText("Falsch geraten!");
-        }
 
-        if (userNumber < myNumber){
-            System.out.println("Deine Zahl ist zu klein.");
-        } else if ( userNumber == myNumber) {
-            System.out.println("Supi!");
+            text.setText("Richtig geraten!! :-) Du hast " + tries + " Versuche gebraucht!");
+
+        } else {
+            tries++;
+            if (userNumber < myNumber) {
+                text.setText("Leider falsch geraten! :-( \n Deine Zahl ist zu klein.");
+
+            } else {
+                text.setText("Leider falsch geraten! :-( \n Deine Zahl ist zu groß.");
+            }
+
+            // nextRound();
+            // Zeile oberhalb in UI nicht notwendig, denn Eingabe über CMD Zeile obsolet
+
         }
-        else {
-            System.out.println("Deine Zahl ist zu groß.");
-        }
-        // nextRound();
     }
 }
